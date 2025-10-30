fde1d9506806cdaf41f780bf06a512e29ec713d2
Marisol-Mancera <marisolmancera10@gmail.com>
2025-10-30 21:03:15 +0100

    FP-35<feat(admin): add AdminPage with profile display and logout functionality>

--- src/app/routes/Router.jsx
+++ src/app/routes/Router.jsx
@@ -4,6 +4,7 @@ import CredentialsLayout from '/src/shared/layout/CredentialsLayout.jsx'
 import HomePage from '/src/features/home/HomePage.jsx'
 import RegisterPage from '/src/features/auth/pages/RegisterPage.jsx'
 import LoginPage from '/src/features/auth/pages/LoginPage.jsx'
+import AdminPage from '/src/features/admin/pages/AdminPage.jsx'
 import PrivateRoute from '../../shared/components/auth/PrivateRoute.jsx'
 
 /**
@@ -19,7 +20,7 @@ const router = createBrowserRouter([
     children: [
       { path: '/', element: <HomePage /> },
       // Rutas protegidas (requieren autenticación)
-      // { path: '/admin', element: <PrivateRoute><AdminPage /></PrivateRoute> },
+      { path: '/admin', element: <PrivateRoute><AdminPage /></PrivateRoute> },
       // { path: '/mascotas', element: <PrivateRoute><MascotasPage /></PrivateRoute> },
     ],
   },
